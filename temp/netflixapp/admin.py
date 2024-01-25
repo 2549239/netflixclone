4264556ea68a9262a140b4e7a9e23f44abd7c9d4
Ogechukwu Ike <2549239@dundee.ac.uk>
2024-01-25 12:52:25 +0000

    cached netflixprj

---
+++ temp/netflixapp/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from netflixapp.models import Movie, Video, Profile, Customuser
+
+
+
+
+admin.site.register(Movie)
+admin.site.register(Video)
+admin.site.register(Profile)
+admin.site.register(Customuser)
+
+# Register your models here.
